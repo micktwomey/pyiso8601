77e70ce90a655ff93697b6c8437f7b11ffb8c93c
Michael Twomey <mick@twomeylee.name>
2021-11-07 19:20:31 +0000

    Add sphinx-build to nox build

--- noxfile.py
+++ noxfile.py
@@ -8,6 +8,12 @@ def lint(session: nox.Session):
     session.run("mypy", "iso8601")
 
 
+@nox.session(reuse_venv=True)
+def docs(session: nox.Session):
+    session.install(".", "Sphinx")
+    session.run("sphinx-build", "docs", "docs/_build")
+
+
 @nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10", "pypy3"], reuse_venv=True)
 def test(session: nox.Session):
     session.install(".")
