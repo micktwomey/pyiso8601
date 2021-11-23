46baabb7c6b3619d9ac8efd90aebaeb32d25b97d
Michael Twomey <mick@twomeylee.name>
2021-11-23 14:15:52 +0000

    Add an `__all__` to `__init.py__`
    Addresses https://github.com/micktwomey/pyiso8601/issues/17
    
    Add `docs/example.py` with a mypy check to catch similar issues.
    
    Run `pre-commit autoupate`
    
    Run `poetry update`

--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,17 @@ import nox
 
 @nox.session(reuse_venv=True)
 def lint(session: nox.Session):
-    session.install("pytest", "hypothesis", "pytz", "black", "mypy")
+    session.install("pytest", "hypothesis", "pytz", "black", "mypy", "isort")
+    session.run("isort", "--check", "--diff", "iso8601")
     session.run("black", "--check", "--diff", "iso8601")
-    session.run("mypy", "iso8601")
+    session.run("mypy", "--strict", "iso8601")
+
+
+@nox.session(reuse_venv=True)
+def check_example(session: nox.Session):
+    session.install(".", "mypy")
+    session.run("mypy", "--strict", "docs/example.py")
+    session.run("python", "docs/example.py")
 
 
 @nox.session(reuse_venv=True)
